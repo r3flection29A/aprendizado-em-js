ffda2700fe9498798e800e86f0dbf7f46cc652c8
yaamat0 <yamatofoda@gmail.com>
2022-01-12 13:36:09 -0300

    alterando o script.js

--- mod5/manipulando-dados/script.js
+++ mod5/manipulando-dados/script.js
@@ -37,7 +37,30 @@ console.log(String(number))
 
 let word = "Cacetada"
 console.log(word.length)
-let number = 123
+let n1 = 123
 console.log(String(number).length)
 
+// Transformar um número quebrado com 2 casas decimais e trocar ponto por vírgula
+
+let n = 208204284022.424224
+console.log(n.toFixed(2).replace(".", ","))
+// toFixed -> fixa duas casas decimais
+// replace -> faz o replace pelo o que o user deseja
+
+// Transformar letras minúsculas em maiúsculas e o contrário
+
+let w = "Programar é legal!"
+console.log(w.toUpperCase())
+console.log(w.toLowerCase())
+
+// Separe um texto que contem espaços, em um novo array onde cada texto
+// é uma posição do array. Depois disso, transforme o array em um texto e onde eram os espaços coloque _
+
+let phrase = "Eu quero viver um amor!"
+let myArray = phrase.split(" ")
+let phraseWithUnderscore = myArray.join("_")
+console.log(phraseWithUnderscore)
+
+// split -> transforma um array com espaços
+// join -> implementa um caractere
 
